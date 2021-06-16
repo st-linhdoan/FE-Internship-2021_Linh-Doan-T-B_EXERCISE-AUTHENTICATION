28501175e6c63a48b165f84b0d63fb8bc35776bf
st-linhdoan <linh.doan@supremetech.vn>
2021-06-16 19:25:06 +0700

    login

--- src/app/pages/Login.tsx
+++ src/app/pages/Login.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { useTranslation } from "react-i18next";
 
 import Button from "../share/component/partials/Button";
 import Input from "../share/component/modules/Input";
 
 const Login = () => {
+  const { t } = useTranslation();
+
   const {
     register,
     handleSubmit,
@@ -12,36 +15,47 @@ const Login = () => {
   } = useForm();
 
   const onSubmit = (data: any) => console.log(data);
+
+  const listRender = [
+    {
+      placehoderName: "Email",
+      validate: register("email", { required: "You must specify a email" }),
+      type: "email",
+      errors: errors.email,
+      para: "",
+    },
+    {
+      placehoderName: "Password",
+      validate: register("password", {
+        required: "You must specify a password",
+        pattern: {
+          value: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,}$/,
+          message: "Invalid password format",
+        },
+        minLength: {
+          value: 8,
+          message: "Password must have at least 8 characters",
+        },
+      }),
+      type: "password",
+      errors: errors.password,
+      para: "",
+    },
+  ];
   return (
-    <div className="register">
-      <div className="register-inner">
+    <div className="form login">
+      <div className="form-inner login-inner">
         <form onSubmit={handleSubmit(onSubmit)}>
           <h2 className="title">Login</h2>
-          <Input
-            placehoderName={"Email"}
-            validate={register("email", {
-              required: "You must specify a age email",
-            })}
-            type={"email"}
-            errors={errors.email}
-          />
-          <Input
-            placehoderName={"Password"}
-            validate={register("password", {
-              required: "You must specify a password",
-              pattern: {
-                value:
-                  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,}$/,
-                message: "Invalid password format",
-              },
-              minLength: {
-                value: 8,
-                message: "Password must have at least 8 characters",
-              },
-            })}
-            type={"password"}
-            errors={errors.password}
-          />
+          {listRender.map((item) => (
+            <Input
+              placehoderName={t(item.placehoderName)}
+              validate={item.validate}
+              type={item.type}
+              errors={item.errors}
+              para={t(item.para)}
+            />
+          ))}
           <Button className={"btn btn-default"} nameBtn={"Login"} />
         </form>
       </div>
