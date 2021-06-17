9f1791be46be3db2e59837ab5498bf732f3ce88d
st-linhdoan <linh.doan@supremetech.vn>
2021-06-17 10:51:27 +0700

    create login and validate

--- src/app/pages/Login.tsx
+++ src/app/pages/Login.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
+import { Link } from 'react-router-dom';
 
 import Button from "../share/component/partials/Button";
 import Input from "../share/component/modules/Input";
@@ -18,23 +19,23 @@ const Login = () => {
 
   const listRender = [
     {
-      placehoderName: "Email",
-      validate: register("email", { required: "You must specify a email" }),
+      placehoderName: "filed.email",
+      validate: register("email", { required: "errors.required.email" }),
       type: "email",
       errors: errors.email,
       para: "",
     },
     {
-      placehoderName: "Password",
+      placehoderName: "filed.password",
       validate: register("password", {
-        required: "You must specify a password",
+        required: "errors.required.password",
         pattern: {
           value: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,}$/,
-          message: "Invalid password format",
+          message: "erroes.password.format",
         },
         minLength: {
           value: 8,
-          message: "Password must have at least 8 characters",
+          message: "errors.password.characters",
         },
       }),
       type: "password",
@@ -57,6 +58,7 @@ const Login = () => {
             />
           ))}
           <Button className={"btn btn-default"} nameBtn={t("login-title")} />
+          <p className="link-sign-up">Do not have an account? <Link to="/register">Sign up</Link> </p>
         </form>
       </div>
     </div>
