2faefc8ff63019400cac8888c0a8a08bda1b5d93
st-linhdoan <linh.doan@supremetech.vn>
2021-06-18 11:34:05 +0700

    fix validate register

--- src/app/pages/Register.tsx
+++ src/app/pages/Register.tsx
@@ -1,107 +1,116 @@
-import React, { useRef } from "react";
-import { useForm } from "react-hook-form";
-import { useTranslation } from "react-i18next";
-import { Link } from "react-router-dom";
-import Input from "../share/component/modules/Input";
-import Button from "../share/component/partials/Button";
+import React, { useRef } from 'react';
+import { useForm } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import Input from '../share/component/modules/Input';
+import Button from '../share/component/partials/Button';
 
 const Register = () => {
   const { t } = useTranslation();
-
   const {
     register,
     handleSubmit,
     watch,
-    formState: { errors },
-  } = useForm();
+    formState: { errors,  isDirty, isValid  },
+  } = useForm({ mode: 'onBlur', reValidateMode: 'onBlur' });
+  
   const password = useRef({});
-  password.current = watch("password", "");
+  password.current = watch('password', '');
 
   const onSubmit = (data: any) => console.log(data);
 
   const listRender = [
     {
-      placehoderName: "filed.name",
-      validate: register("name", { required: "errors.required.name" }),
-      type: "text",
+      placehoderName: 'filed.name',
+      validate: register('name', { required: 'errors.required.name' }),
+      type: 'text',
       errors: errors.name,
-      para: "",
+      para: '',
     },
     {
-      placehoderName: "filed.email",
-      validate: register("email", { required: "errors.required.email" }),
-      type: "email",
+      placehoderName: 'filed.email',
+      validate: register('email', { 
+        required: 'errors.required.email', 
+         pattern: {
+          value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+          message: 'errors.email.format',
+        }, }),
+      type: 'text',
       errors: errors.email,
-      para: "",
+      para: '',
     },
     {
-      placehoderName: "filed.age",
-      validate: register("age", {
-        required: "errors.required.age",
+      placehoderName: 'filed.age',
+      validate: register('age', {
+        required: 'errors.required.age',
         min: {
           value: 16,
-          message: "errors.age.min",
+          message: 'errors.age.min',
         },
         max: {
           value: 60,
-          message: "errors.age.max",
+          message: 'errors.age.max',
         },
+        pattern:{
+          value: /\d+/,
+          message: 'errors.age.format',
+        } 
       }),
-      type: "text",
+      type: 'text',
       errors: errors.age,
-      para: "",
+      para: '',
     },
     {
-      placehoderName: "filed.adrress",
-      validate: register("address", {
-        required: "errors.required.address",
+      placehoderName: 'filed.adrress',
+      validate: register('address', {
+        required: 'errors.required.address',
       }),
-      type: "text",
+      type: 'text',
       errors: errors.address,
-      para: "",
+      para: '',
     },
     {
-      placehoderName: "filed.password",
-      validate: register("password", {
-        required: "errors.required.password",
+      placehoderName: 'filed.password',
+      validate: register('password', {
+        required: 'errors.required.password',
         pattern: {
           value: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,}$/,
-          message: "errors.password.format",
+          message: 'errors.password.format',
         },
         minLength: {
           value: 8,
-          message: "errors.password.characters",
+          message: 'errors.password.characters',
         },
       }),
-      type: "password",
+      type: 'password',
       errors: errors.password,
-      para: "",
+      para: '',
     },
     {
-      placehoderName: "filed.confirm-password",
-      validate: register("confirm_password", {
+      placehoderName: 'filed.confirm-password',
+      validate: register('confirm_password', {
         validate: (value) =>
-          value === password.current || "errors.password.match",
+          value === password.current || 'errors.password.match',
       }),
-      type: "password",
+      type: 'password',
       errors: errors.confirm_password,
-      para: "",
+      para: '',
     },
     {
-      placehoderName: "",
-      validate: register("checkbox", {
-        required: "errors.required.accept",
+      placehoderName: '',
+      validate: register('checkbox', {
+        required: 'errors.required.accept',
       }),
-      type: "checkbox",
+      type: 'checkbox',
       errors: errors.checkbox,
-      para: "terms-service",
+      para: 'question.terms-service',
     },
   ];
   return (
     <div className="form register-form">
       <div className="form-inner register-inner">
         <form onSubmit={handleSubmit(onSubmit)}>
-          <h2 className="title">{t("register-title")}</h2>
+          <h2 className="title">{t('register-title')}</h2>
           {listRender.map((item) => (
             <Input
               placehoderName={t(item.placehoderName)}
@@ -111,8 +120,8 @@ const Register = () => {
               para={t(item.para)}
             />
           ))}
-          <Button className={"btn btn-default"} nameBtn={t("btn.register")} />
-          <p className="link-sign-in">Already have an account? <Link to='/login'>Sign In</Link></p>
+          <Button disabled={!isValid || !isDirty}  className={!isValid || !isDirty ? "btn btn-disable" : "btn btn-default"} nameBtn={t('btn.register')} />
+          <p className="link-sign-in">{t('question.account')}<Link to='/login'>{t('link-name.sign-in')}</Link></p>
         </form>
       </div>
     </div>
